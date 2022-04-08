99f3efcd47c72f9948da904c7c75ab10a85d94a9
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-08 16:15:21 -0300

    s

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,17 @@
 const gridCell = document.querySelectorAll('.cell');
+let gameBoard = (input, index) => {
+    const mark = (input, index) => {
+        
+    }
+    return {input};
+}
 
 gridCell.forEach(cell => {
 	cell.addEventListener('click', function () {
 		cell.textContent = 'x';
+        gameBoard[parseInt(cell.id)] = cell.textContent;
+        console.log(gameBoard);
 	});
 });
+
 
