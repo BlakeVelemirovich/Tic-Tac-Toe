c08538446fc0726d1df88aab72b4029eaa8838ab
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-07 22:02:17 -0300

    grid can take input

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,8 @@
 const gridCell = document.querySelectorAll('.cell');
 
 gridCell.forEach(cell => {
-    cell.addEventListener('click', function() {
-        const choice = document.createTextNode('x');
-    });
+	cell.addEventListener('click', function () {
+		cell.textContent = 'x';
+	});
 });
+
