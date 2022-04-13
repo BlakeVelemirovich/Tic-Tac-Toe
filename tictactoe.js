82b4bf308a175416d2dccd3b12dce33b13db3c60
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-13 12:04:24 -0300

    Added player selection functionality

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,29 @@
 const gridCell = document.querySelectorAll('.cell');
-let gameBoard = (input, index) => {
-    const mark = (input, index) => {
-        
+
+// Player Factory
+const players = (name, marker) => {
+
+    // Array and corresponding method for player selection
+    let selections = [];
+    const playerSelection = (selection) => {
+
+        selections[selection] = marker;
     }
-    return {input};
+
+    // Function to check for wins with an array that contains all win conditions
+    const didPlayerWin = () => {
+
+        // todo
+    }
+
+    return {name, marker, playerSelection};
 }
 
+// Defining players using object function factory
+const playerOne = players('Player 1', 'x');
+const playerTwo = players('Player 2', 'o');
+
+// Each cell is a div in HTML, event listener for clicks
 gridCell.forEach(cell => {
 	cell.addEventListener('click', function () {
 		cell.textContent = 'x';
@@ -13,5 +31,3 @@ gridCell.forEach(cell => {
         console.log(gameBoard);
 	});
 });
-
-
